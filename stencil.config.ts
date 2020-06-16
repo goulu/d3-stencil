84bd07b78d31d1d040800097080e2e07c9da9f7b
Philippe Guglielmetti <github@goulu.net>
2020-06-16 11:42:43 +0200

    Stencil 1.12.2 OK, simplified ...

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,17 +1,19 @@
 import { Config } from '@stencil/core';
-import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'd3-stencil',
-  outputTargets: [{ type: 'dist' }],
-  plugins: [
-    sass({
-      injectGlobalPaths: ['src/scss/objects.scss'],
-    }),
-  ],
-  testing: {
-    testEnvironmentOptions: {
-      userAgent: 'Agent/007',
+  taskQueue: 'async',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
     },
-  },
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ]
 };
